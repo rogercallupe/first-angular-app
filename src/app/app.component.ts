1b75f1fcd717f5aef071743340b481961d00cdb9
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-22 08:25:39 -0500

    Udemy-Angular-user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent]
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
