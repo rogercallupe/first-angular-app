9679ef0ced4b3e54537b7f2ebecd7c4a16cc2614
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-23 15:17:12 -0500

    Udemy-Angular-output-task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input ({required: true}) id!: string;
   @Input ({required: true}) avatar!: string;
   @Input ({required:true}) name!: string;
-  @Output() select = new EventEmitter<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
