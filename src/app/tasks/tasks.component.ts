de6b51559651b77a3ce6368b9143baf496be1972
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-26 15:33:05 -0500

    Udemy-Angular-new-add-task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component'; // Importing the TaskComponent
+import { NewTaskComponent } from './new-task/new-task.component'; // Importing the NewTaskComponent
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, NewTaskComponent] // Importing the TaskComponent and NewTaskComponent
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string; // Using ! to assert that id will be provided
   @Input({required: true}) name!: string;
+  isAddingTask= false;
 
 tasks = [
     { id: 't1',
@@ -42,5 +44,9 @@ tasks = [
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
   }
 }
