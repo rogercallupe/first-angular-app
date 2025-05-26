b7656613f9c133a1754ffbfa43a42579c5b063be
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-26 11:33:45 -0500

    Udemy-Angular-@ngFor,@ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf} from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS }  from './dummy-users';
@@ -9,11 +10,12 @@ import { TasksComponent } from './tasks/tasks.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent]
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf]
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  //selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
