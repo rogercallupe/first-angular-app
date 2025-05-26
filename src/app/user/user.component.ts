1b86458480c5d3e6d85554e471c160a19a985a86
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-26 14:34:27 -0500

    Udemy-Angular-active-CSS

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,12 +9,8 @@ import { type User } from './user.model'; // Assuming you have a User model defi
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input ({required: true}) id!: string;
-  // @Input ({required: true}) avatar!: string;
-  // @Input ({required:true}) name!: string;
-  // or
   @Input ({required: true}) user!: User;
-
+  @Input ({required: true}) selected!: boolean;
   @Output() select = new EventEmitter();
 
   get imagePath() {
