37fa8ecd533c10a7766945053f0c16c7a2e7eaab
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-26 13:35:38 -0500

    Udemy-Angular-User-Task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,42 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component'; // Importing the TasksComponent
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent]
 })
 export class TasksComponent {
-  @Input () name?: string;
-  // can delete ? of name and use the one below 
- // @Input () name: string | undefined;
+  @Input({required: true}) userId!: string; // Using ! to assert that id will be provided
+  @Input({required: true}) name!: string;
 
+ tasks = [
+    { id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Lear all the basic and advanced features of Angular & how to apply them in real-world applications.',
+      dueDate: '2025-12-31'
+    },
+    {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
