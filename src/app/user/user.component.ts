e978d2560acc7c80e5e01c8aef3bf259c9c1eeca
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-26 09:32:00 -0500

    Udemy-Angular-typeScript-notes-type_alias&interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,19 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+// or
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,11 +25,7 @@ export class UserComponent {
   // @Input ({required: true}) avatar!: string;
   // @Input ({required:true}) name!: string;
   // or
-  @Input ({required: true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-    };
+  @Input ({required: true}) user!: User;
 
   @Output() select = new EventEmitter();
 
